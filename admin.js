face89261295e098a5447bbe6d9e3df3eaae2dab
Hezza M. Ramadan <hezzamr691@gmail.com>
2026-01-29 19:48:45 +0700

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,27 @@
+const ADMIN_SECRET = 'ADMIN123'; // ganti sendiri
+
+
+const supabaseUrlA = 'https://XXXX.supabase.co';
+const supabaseKeyA = 'sb_secret_wQ2OtTs7TIdXm4r5kdygpQ_OlHuqlrM';
+const adminSB = window.supabase.createClient(supabaseUrlA, supabaseKeyA);
+
+
+function loginAdmin() {
+const key = document.getElementById('adminKey').value;
+if (key === ADMIN_SECRET) {
+document.getElementById('adminPanel').classList.remove('hidden');
+} else {
+alert('Admin key salah');
+}
+}
+
+
+async function resetUptime() {
+await adminSB
+.from('uptime')
+.update({ start_time: Date.now() })
+.eq('id', 1);
+
+
+alert('Uptime berhasil direset');
+}
