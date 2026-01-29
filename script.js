3708d01b36257fa06f8d0d7a8d79a4f8f889df27
Hezza M. Ramadan <hezzamr691@gmail.com>
2026-01-29 19:26:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const supabaseUrl = 'https://lfjormecnyrhplwfgyaq.supabase.co';
 const supabaseKey = 'sb_publishable_XUvHyt_TEzDGqUBRP1flwg_GshP2c1I';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+const sb = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
 
 async function updateUptime() {
-  const { data, error } = await supabase
+  const { data, error } = await sb
     .from('uptime')
     .select('start_time')
     .eq('id', 1)
@@ -16,9 +20,9 @@ async function updateUptime() {
 
   const diff = Date.now() - data.start_time;
 
-  const h = String(Math.floor(diff / 3600000)).padStart(2, '0');
-  const m = String(Math.floor((diff % 3600000) / 60000)).padStart(2, '0');
-  const s = String(Math.floor((diff % 60000) / 1000)).padStart(2, '0');
+  const h = String(Math.floor(diff / 3600000)).padStart(2,'0');
+  const m = String(Math.floor((diff % 3600000) / 60000)).padStart(2,'0');
+  const s = String(Math.floor((diff % 60000) / 1000)).padStart(2,'0');
 
   document.getElementById('time').innerText = `${h}:${m}:${s}`;
 }
